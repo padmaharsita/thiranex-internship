38f09adbc95a8039053076d6dc5c2d401e0e10a5
padmaharsita <padma.harsita@gmail.com>
2026-05-09 13:00:28 +0530

    Implement deposit, withdraw, and balance check features
    Added deposit, withdraw, and check balance methods to the Bank class. Enhanced user interaction for managing account transactions.

--- 1main.cpp
+++ 1main.cpp
@@ -1,50 +1,92 @@
 #include<iostream>
-#include<fstream>
 using namespace std;
-
 class Bank
 {
-    int account_no;
+    int account_number;
     char name[30];
     float balance;
-
 public:
-
     void create()
     {
-        cout<<"Enter account number: ";
-        cin>>account_no;
-
-        cout<<"Enter name: ";
+        cout<<"Enter Account Number: ";
+        cin>>account_number;
+        cout<<"Enter Name: ";
         cin>>name;
-
-        cout<<"Enter balance: ";
+        cout<<"Enter Balance: ";
         cin>>balance;
     }
-
     void display()
     {
-        cout<<"\nAccount Number: "<<account_no;
+        cout<<"\nAccount Number: "<<account_number;
         cout<<"\nName: "<<name;
-        cout<<"\nBalance: "<<balance;
+        cout<<"\nBalance: "<<balance<<endl;
+    }
+    void deposit()
+    {
+        float amount;
+        cout<<"Enter Amount: ";
+        cin>>amount;
+        balance += amount;
+        cout<<"Amount Deposited Successfully\n";
+        cout<<"Current Balance: "<<balance<<endl;
+    }
+    void withdraw()
+    {
+        float amt;
+        cout<<"Enter Amount: ";
+        cin>>amt;
+        if(amt > balance)
+        {
+            cout<<"Insufficient Balance\n";
+        }
+        else
+        {
+            balance -= amt;
+
+            cout<<"Amount Withdrawn Successfully\n";
+            cout<<"Current Balance: "<<balance<<endl;
+        }
     }
 
-    void save()
+    void checkBalance()
     {
-        ofstream file("bank.dat", ios::binary | ios::app);
-        file.write((char*)this, sizeof(*this));
-        file.close();
+        cout<<"Current Balance: "<<balance<<endl;
     }
 };
-
 int main()
 {
     Bank b;
-
+    int choice;
     b.create();
-    b.save();
-
-    cout<<"\nAccount created successfully!\n";
-
+    do
+    {
+        cout<<"\n1.Deposit";
+        cout<<"\n2.Withdraw";
+        cout<<"\n3.Check Balance";
+        cout<<"\n4.Display Details";
+        cout<<"\n5.Exit";
+        cout<<"\nEnter Choice: ";
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+                b.deposit();
+                break;
+            case 2:
+                b.withdraw();
+                break;
+            case 3:
+                b.checkBalance();
+                break;
+            case 4:
+                b.display();
+                break;
+            case 5:
+                cout<<"Exiting!";
+                break;
+            default:
+                cout<<"Invalid Choice";
+        }
+    } while(choice != 5);
     return 0;
 }
